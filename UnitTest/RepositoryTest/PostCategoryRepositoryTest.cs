117a22067eb7fc78886f7396e32d9a01613479d8
ngocdungduong <dungduong.ftu2@gmail.com>
2019-12-26 18:17:52 +0700

    Add unit test and learn about mock test

---
+++ UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,53 @@
+﻿using Data.Infrastructure;
+using Data.Repositories;
+using Data.Repositories.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IUnitOfWork unitOfWork;
+        IPostCategoryRepository objRepository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            unitOfWork = new UnitOfWork(dbFactory);
+            objRepository = new PostCategoryRepository(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll();
+            Assert.AreEqual(1, list.Count());
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Add()
+        {
+            PostCategory obj = new PostCategory()
+            {
+                Name = "test add",
+                Alias = "test add",
+                Status = true
+            };
+
+            var result = objRepository.Add(obj);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.ID);
+        }
+    }
+}
---
+++ UnitTest/ServiceTest/PostCategoryServiceTest.cs
@@ -0,0 +1,103 @@
+﻿using Data.Infrastructure;
+using Data.Repositories;
+using Data.Repositories.Interface;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Models;
+using Moq;
+using Service;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.ServiceTest
+{
+    [TestClass]
+    public class PostCategoryServiceTest
+    {
+        private Mock<IPostCategoryRepository> _mockRepository;
+        private Mock<IUnitOfWork> _mockUnitOfWork;
+        IPostCategoryService _objService;
+
+        List<PostCategory> listObj;
+
+        /// <summary>
+        /// Mục đích của cái mock này là tạo 1 đối tượng giả
+        /// sau đó phần tham số truyền vào cho cái service thì mình chỉ cần mock.object là sẽ lấy được
+        /// một INSTANCE của cái mock mới tạo giả đó
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<IPostCategoryRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _objService = new PostCategoryService(_mockRepository.Object, _mockUnitOfWork.Object);
+            listObj = new List<PostCategory>()
+            {
+                new PostCategory(){Name = "Danh sách 1", Alias = "Category 1", Status = true},
+                new PostCategory(){Name = "Danh sách 2", Alias = "Category 2", Status = true},
+                new PostCategory(){Name = "Danh sách 3", Alias = "Category 3", Status = true}
+            };
+        }
+
+        /// <summary>
+        /// 1 hàm test mock như vậy sẽ có 3 bước: 
+        /// SET UP METHOD - CALL ACTION - COMPARE
+        /// </summary>
+        [TestMethod]
+        public void PostCategory_Service_GetAll()
+        {
+            //set up method
+            _mockRepository.Setup(m => m.GetAll(null)).Returns(listObj);
+
+            //call action
+            var result = _objService.GetAll() as List<PostCategory>;
+
+            //compare
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Add()
+        {
+            PostCategory obj = new PostCategory()
+            {
+                Name = "ADD NEW",
+                Alias = "Add New",
+                Status = true
+            };
+
+            _mockRepository.Setup(m => m.Add(obj)).Returns((PostCategory p) =>
+            {
+                p.ID = 1;
+                return p;
+            });
+
+            var result = _objService.Add(obj);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.ID);
+        }
+
+        [TestMethod]
+        public void PostCategory_Service_Delete()
+        {
+            PostCategory obj = new PostCategory()
+            {
+                ID = 2,
+                Name = "Xóa",
+                Alias = "Delete",
+                Status = true
+            };
+
+            _mockRepository.Setup(m => m.Delete(obj)).Returns((PostCategory p)=> { return null; });
+
+            var result = _objService.Delete(obj);
+
+            Assert.IsNull(result);
+        }
+    }
+}
